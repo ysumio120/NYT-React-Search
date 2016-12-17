9f12edd011817132cd6091c1de019063c91d305b
Yuuta Sumioka <ysumio120@yahoo.com>
2016-12-17 01:05:21 -0500

    config for mLab db

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
 var mongoose = require("mongoose");
 
 // Local Connection
-mongoose.connect("mongodb://localhost:27017/nytreact", function(err) {
-	if(err) throw err;
-	console.log('database connected');
-});
+// mongoose.connect("mongodb://localhost:27017/nytreact", function(err) {
+// 	if(err) throw err;
+// 	console.log('database connected');
+// });
 
 // mLab Connection (Heroku)
-// mongoose.connect("mongodb://heroku_h8wn5m54:q9uhgn7ejhj91em7c7hjkplr2b@ds129038.mlab.com:29038/heroku_h8wn5m54", function(err) {
-//   if(err) throw err;
-//   console.log('database connected');
-// })
+mongoose.connect("mongodb://heroku_nmkzfgp1:gutihm1dkfmaihh3vtbolcqoma@ds113628.mlab.com:13628/heroku_nmkzfgp1", function(err) {
+  if(err) throw err;
+  console.log('database connected');
+})
